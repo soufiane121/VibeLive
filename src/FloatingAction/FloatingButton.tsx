0bf52b59c56e8996083b36c65f53785ab19af999
soufiane121 <soufiane.oucherrou@gmail.com>
2025-02-23 19:03:46 -0500

    add mic icons as images on action btns

--- src/FloatingAction/FloatingButton.tsx
+++ src/FloatingAction/FloatingButton.tsx
@@ -1,70 +1,43 @@
 // Floating Action Button Example in React Native
 // Main button shows other action buttons in a half-circle when clicked
 
-import React, {useState} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
-import { emojis } from '../Utils/emojis';
-import { HappyFaceEmojiIcon } from '../UIComponents/Icons';
+import React, {useState, Suspense} from 'react';
+import {View, TouchableOpacity, Text, Image, StyleSheet} from 'react-native';
+import {emojis} from '../Utils/emojis';
+import {HappyFaceEmojiIcon} from '../UIComponents/Icons';
 // import {Ionicons} from '@expo/vector-icons';
 
 export default function FloatingActionButton() {
   const [isOpen, setIsOpen] = useState(false);
 
   const buttons = [
-    { label: 'Like', emoji: emojis.like},
-    { label: 'Cheers', emoji: emojis.beer},
-    { label: 'Hot', emoji: emojis.hot},
-    { label: 'Music', emoji: emojis.music},
-    { label: 'Angry', emoji: emojis.angery},
-    { label: 'Dislike', emoji: emojis.dislike},
+    {label: 'Like', emoji: emojis.like},
+    {label: 'Cheers', emoji: emojis.beer},
+    {label: 'Hot', emoji: emojis.hot},
+    {label: 'Music', emoji: emojis.music},
+    {label: 'Boring', emoji: emojis.angery},
+    {label: 'Dislike', emoji: emojis.dislike},
   ];
 
   return (
     <View style={{}}>
       {/* Main Floating Button */}
       <TouchableOpacity
-        style={{
-          backgroundColor: '#6200ea',
-          borderRadius: 30,
-          width: 60,
-          height: 60,
-          justifyContent: 'center',
-          alignItems: 'center',
-          
-        }}
+        style={styles.containr}
         onPress={() => setIsOpen(!isOpen)}>
-        {/* <Ionicons name={isOpen ? 'close' : 'add'} size={30} color="#fff" /> */}
-        <HappyFaceEmojiIcon size={30} color="#fff" />
+        <HappyFaceEmojiIcon size={30} style={styles.parentIcon}/>
       </TouchableOpacity>
 
       {/* Action Buttons - Half Circle Around Main Button */}
       {isOpen && (
         <View
-          style={{
-            position: 'absolute',
-            bottom: 70,
-            right: 10,
-            alignItems: 'center',
-            borderWidth: 2,
-            borderColor: '#6200ea',
-
-          }}>
+          style={styles.actionIconsWrapper}>
           {buttons.map((btn, index) => (
             <TouchableOpacity
               key={index}
-              style={{
-                marginBottom: 10,
-                alignItems: 'center',
-                backgroundColor: '#03DAC6',
-                borderRadius: 30,
-                padding: 10,
-                width: 50,
-                height: 50,
-                justifyContent: 'center',
-              }}>
-              {/* <Ionicons name={btn.icon} size={24} color="#fff" /> */}
-              <Text>{btn.emoji}</Text>
-              <Text style={{color: '#fff'}}>{btn.label}</Text>
+              style={styles.actionIconsContainer}>
+              <Image source={btn.emoji} style={styles.img} />
+              {/* <Text style={{color: '#fff'}}>{btn.label}</Text> */}
             </TouchableOpacity>
           ))}
         </View>
@@ -72,3 +45,34 @@ export default function FloatingActionButton() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  containr: {
+    borderRadius: 30,
+    width: 60,
+    height:70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  parentIcon: {
+    color: '#fffff',
+  },
+  actionIconsWrapper: {
+    position: 'absolute',
+    bottom: 70,
+    right: 10,
+    alignItems: 'center',
+  },
+  actionIconsContainer: {
+    alignItems: 'center',
+    borderRadius: 30,
+    // padding: 10,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+  },
+  img: {
+    width: 25,
+    height: 25,
+  },
+});
--- src/Utils/emojis.js
+++ src/Utils/emojis.js
@@ -1,8 +1,15 @@
+const heart = require('../../assests/heart.png');
+const beer = require('../../assests/beer.png');
+const hot = require('../../assests/hot.png');
+const music = require('../../assests/music.png');
+const dislike = require('../../assests/dislike.png');
+const angery = require('../../assests/yawning.png');
+
 export const emojis = {
-	beer: "🍻",
-	hot: "🔥",
-	like: "❤️",
-	music: "🎶",
-	dislike: "👎",
-	angery: "😡",
+  beer: beer,
+  hot: hot,
+  like: heart,
+  music: music,
+  dislike: dislike,
+  angery: angery,
 };
